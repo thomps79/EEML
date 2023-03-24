b251c95210af88c62727adf377beddc714ca84e3
thomps79 <thomps79@students.rowan.edu>
2023-03-24 17:26:05 -0400

    Initial GAN model

---
+++ EEGAN.py
@@ -0,0 +1,247 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+import os
+import numpy as np
+import math
+import cv2
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from PIL import Image
+
+png_dir = "C:\\Users\\awt24\\Desktop\\files\\"
+
+def parseFile(file):
+    world = np.zeros((200,200,3))
+    with open(file, 'rb') as f:
+        
+        # Read the binary data into a buffer
+        buffer = f.read()
+        i = 0
+        for x in range(200):
+            for y in range(200):
+                world[x][y][2] = buffer[i]
+                world[x][y][1] = buffer[i+1]
+                world[x][y][0] = buffer[i+2]
+                i += 3
+			
+    return world
+
+
+"""
+print("loading worlds")
+files = [x for x in os.listdir("C:\\Users\\awt24\\Downloads\\ArchivEE_1.0.0\\output\\")]
+for file in files:
+    half = cv2.resize(parseFile(f"C:\\Users\\awt24\\Downloads\\ArchivEE_1.0.0\\output\\{file}"), (0, 0), fx = 0.25, fy = 0.25)
+    cv2.imwrite(f"files\\{file[0:-2]}png", half)
+os.exit()
+"""
+
+THE_SIZE = 50
+
+# Data Loader
+
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+class PNGDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_files = [f for f in os.listdir(root_dir) if f.endswith('.png')]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = os.path.join(self.root_dir, self.image_files[idx])
+        image = Image.open(img_path).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image
+
+
+class Generator(nn.Module):
+    def __init__(self, img_shape, opt):
+        super(Generator, self).__init__()
+        self.img_shape = img_shape
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(opt.latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), *self.img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_shape):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+        return validity
+
+def main():
+    # Define any desired transformations
+    transformations = transforms.Compose([
+        transforms.Resize((THE_SIZE, THE_SIZE)), # Should already be 25x25
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    batch_size = 64
+    num_workers = 4
+    shuffle = True
+
+    dataset = PNGDataset(root_dir=png_dir, transform=transformations)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    ##
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+    parser.add_argument("--img_size", type=int, default=THE_SIZE, help="size of each image dimension")
+    parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+    parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+    opt = parser.parse_args()
+    print(opt)
+
+    img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+    cuda = True if torch.cuda.is_available() else False
+
+    # Loss function
+    adversarial_loss = torch.nn.BCELoss()
+
+    # Initialize generator and discriminator
+    generator = Generator(img_shape, opt)
+    discriminator = Discriminator(img_shape)
+
+    if cuda:
+        generator.cuda()
+        discriminator.cuda()
+        adversarial_loss.cuda()
+
+    # Configure data loader (done above)
+
+    # Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+    Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+    # ----------
+    #  Training
+    # ----------
+
+    for epoch in range(opt.n_epochs):
+        for i, imgs in enumerate(dataloader):
+
+            # Adversarial ground truths
+            valid = Variable(Tensor(imgs.size(0), 1).fill_(1.0), requires_grad=False)
+            fake = Variable(Tensor(imgs.size(0), 1).fill_(0.0), requires_grad=False)
+
+            # Configure input
+            real_imgs = Variable(imgs.type(Tensor))
+
+            # -----------------
+            #  Train Generator
+            # -----------------
+
+            optimizer_G.zero_grad()
+
+            # Sample noise as generator input
+            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+            # Generate a batch of images
+            gen_imgs = generator(z)
+
+            # Loss measures generator's ability to fool the discriminator
+            g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+            g_loss.backward()
+            optimizer_G.step()
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            optimizer_D.zero_grad()
+
+            # Measure discriminator's ability to classify real from generated samples
+            real_loss = adversarial_loss(discriminator(real_imgs), valid)
+            fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
+
+            d_loss.backward()
+            optimizer_D.step()
+
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            )
+
+            batches_done = epoch * len(dataloader) + i
+            if batches_done % opt.sample_interval == 0:
+                save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+                # Save the model weights and optimizer state
+                torch.save(generator.state_dict(), f"model{batches_done}.pt")
+                torch.save(discriminator.state_dict(), f"model{batches_done}.pt")
+                torch.save(optimizer_G.state_dict(), f"optim{batches_done}.pt")
+                torch.save(optimizer_D.state_dict(), f"optim{batches_done}.pt")
+
+if __name__ == '__main__':
+    main()
